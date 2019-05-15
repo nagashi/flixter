8f039184c54210c0b24f81fc2ac71db21c9ad797
nagashi <ceoriley@gmail.com>
2019-05-15 15:00:04 +0000

    22:Carrierwave Initial

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,22 @@
-class LessonsController < 
-    def show
-       
-    end
-
-    private
+class LessonsController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :require_enrolled_in_current_course 
   
-    helper_method :current_lesson
-    def current_lesson
-      @current_lesson ||= Lesson.find(params[:id])
+  def show 
+    require_enrolled_in_current_course
+  end 
+
+  def require_enrolled_in_current_course 
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You must be enrolled to view this lesson."
     end
+  end 
+
+  private 
+
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end 
+  
 end
